fe96f5b40cbc8222a0a68c6b0fd2ad45895835f4
Julexio <213358137+Julexio@users.noreply.github.com>
2026-09-11 09:13:32 +0000

    Finish Profile

---
+++ main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 125),
+              Icon(Icons.account_circle, color: Colors.deepPurple, size: 125),
+              Text(
+                'Julien Mark Bondoc', style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+              Text(
+                'Bachelor in Science of Information Technology', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.grey),
+              ),
+              SizedBox(height: 125),
+              
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [ Icon(Icons.house_outlined, color: Colors.deepPurple, size: 45), Text('Home', style: TextStyle(fontSize: 21)),],
+                  ),
+                  SizedBox(width: 25),
+                  Column(
+                    children: [Icon(Icons.menu_book, color: Colors.deepPurple, size: 45), Text('Courses', style: TextStyle(fontSize: 21)),],
+                  ),
+                  SizedBox(width: 25),
+                  Column(
+                    children: [Icon(Icons.person_outline_sharp, color: Colors.deepPurple, size: 45),
+                      Text('Profile', style: TextStyle(fontSize: 21)),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 40),
+              Text('Ang Pogi mo Rodney!', style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
